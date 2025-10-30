b3664621a090a991ebc56f2ad50070840b85315b
naveenkumararemanda <34246654+naveenkumararemanda@users.noreply.github.com>
2025-10-30 09:30:39 +0000

    Add JSON fetching functionality and lesson card template comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 
+// {% for lesson in lessons %}
+//     <a href="/lesson/{{ lesson.number }}/" class="lesson-card">
+//         <h3>Lesson {{ lesson.number }}</h3>
+//         <p>{{ lesson.title }}</p>
+//     </a>
+//     {% endfor %}
 
+readJSON();
 
+function readJSON(){
+    fetch("https://drive.google.com/uc?export=download&id=1CdMB_UcmRT_99JItjKU3GzfXLNW1qE7-").then(response => response.json()).then(data => {
+        console.log(data);
+    }).catch(error => {
+        console.error('Error fetching JSON:', error);
+    });
+}
